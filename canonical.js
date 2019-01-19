40034019845812df88098d70db694c1b9425f56d
Nacho Mascort <nacho.mascort@gmail.com>
2019-01-19 21:59:06 +0100

    Updated to ES6

--- canonical.js
+++ canonical.js
@@ -1,27 +1,18 @@
-var canonical = "";
-var links = document.getElementsByTagName("link");
-for (var i = 0; i < links.length; i ++) {
-    if (links[i].getAttribute("rel") === "canonical") {
-        canonical = links[i].getAttribute("href")
-    }
-};
-var url_absolute = window.location.href; 
-var url_relative = window.location.pathname;
-if (canonical)
-{
-	if (url_absolute == canonical || url_relative == canonical) 
-	{
-		console.log(url_absolute + " is the canonical");
-		alert(url_absolute + " is the canonical");
-	}
-	else 
-	{
-		console.log(url_absolute + " is NOT the canonical, the canonical is " + canonical);
-		alert(url_absolute + " is NOT the canonical, the canonical is " + canonical);
+const canonical = document.querySelector("link[rel='canonical']").href;
+
+const url_absolute = window.location.href;
+const url_relative = window.location.pathname;
+
+function checkCanonical(canonical) {
+	if(canonical) {
+		if(url_absolute == canonical || url_relative == canonical) {
+			console.log(url_absolute + " is the canonical");
+		} else {
+			console.log(url_absolute + " is NOT the canonical, the canonical is " + canonical);
+		}
+	} else {
+		console.log("Canonical tag doesn't exist");
 	}
 }
-else 
-{
-	console.log("Canonical tag doesn't exist");
-	alert("Canonical tag doesn't exist");
-};
+
+checkCanonical(canonical);
